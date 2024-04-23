61ace95bb3851b9e98d92ad3d4279646c7a14ecb
CalinCretu <calincretu1412@gmail.com>
2024-04-23 15:47:35 +0200

    ADD - funzione Potenza() e tutte le sue casistiche

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace csharp_calcolatrice
 {
-    internal class CalcoliHelper
+    internal class CalcoliHelper // Polimorfismo: gli stessi metodi possono essere chiamati con tipi di parametro diversi
     {
         // Metodo per la somma di due numeri interi
         public static int Somma(int a, int b)
@@ -78,6 +78,35 @@ namespace csharp_calcolatrice
         public static double Massimo(double a, double b)
         {
             return a > b ? a : b;
+        }
+
+        // Metodo per l'elevamento a potenza
+        public static string Potenza(int valoreBase, int esponente)
+        {
+            if (valoreBase == 0)
+            {
+                // Se la base è zero e l'esponente è zero, restituisco 1 (convenzione informatica)
+                if (esponente == 0)
+                    return "1";
+                //Se la base è zero e l'esponente è negativo, restituisco infinito positivo
+                else if (esponente < 0)
+                    return "Infinito Positivo";
+                // Se la base è zero e l'esponente è diverso da zero, restituisco 0
+                else
+                    return "0";
+            }
+            // Calcolo l'elevamento a potenza e gestisco il caso dell'esponente negativo
+            double risultato = 1;
+            int esponenteAssoluto = ValoreAssoluto(esponente);
+            for (int i = 0; i < esponenteAssoluto; i++)
+            {
+                risultato *= valoreBase;
+            }
+            // Con esponente negativo
+            if (esponente < 0)
+                risultato = 1 / risultato;
+
+            return risultato.ToString();
         }
     }
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -9,24 +9,44 @@
             Console.WriteLine("Somma di 2.5 e 3.7: " + CalcoliHelper.Somma(2.5, 3.7));
 
             // Differenza
-            Console.WriteLine("Differenza tra 8 e 3: " + CalcoliHelper.Differenza(8, 3));
+            Console.WriteLine("\nDifferenza tra 8 e 3: " + CalcoliHelper.Differenza(8, 3));
             Console.WriteLine("Differenza tra 5.2 e 1.8: " + CalcoliHelper.Differenza(5.2, 1.8));
 
             // Moltiplicazione
-            Console.WriteLine("Moltiplicazione tra 4 e 6: " + CalcoliHelper.Moltiplicazione(4, 6));
+            Console.WriteLine("\nMoltiplicazione tra 4 e 6: " + CalcoliHelper.Moltiplicazione(4, 6));
             Console.WriteLine("Moltiplicazione tra 2.5 e 1.5: " + CalcoliHelper.Moltiplicazione(2.5, 1.5));
 
             // Valore assoluto
-            Console.WriteLine("Valore assoluto di -7: " + CalcoliHelper.ValoreAssoluto(-7));
+            Console.WriteLine("\nValore assoluto di -7: " + CalcoliHelper.ValoreAssoluto(-7));
             Console.WriteLine("Valore assoluto di -2.3: " + CalcoliHelper.ValoreAssoluto(-2.3));
 
             // Minimo
-            Console.WriteLine("Minimo tra 10 e 7: " + CalcoliHelper.Minimo(10, 7));
+            Console.WriteLine("\nMinimo tra 10 e 7: " + CalcoliHelper.Minimo(10, 7));
             Console.WriteLine("Minimo tra 3.5 e 6.2: " + CalcoliHelper.Minimo(3.5, 6.2));
 
             // Massimo
-            Console.WriteLine("Massimo tra 12 e 9: " + CalcoliHelper.Massimo(12, 9));
+            Console.WriteLine("\nMassimo tra 12 e 9: " + CalcoliHelper.Massimo(12, 9));
             Console.WriteLine("Massimo tra 4.2 e 8.7: " + CalcoliHelper.Massimo(4.2, 8.7));
+
+            // Potenza
+            //con base intera ed esponente uguale a zero
+            Console.WriteLine("\nPotenza di 5 alla 0: " + CalcoliHelper.Potenza(5, 0));
+            //con base negativa ed esponente uguale a zero
+            Console.WriteLine("\nPotenza di -5 alla 0: " + CalcoliHelper.Potenza(-5, 0));
+            //con base uguale a zero ed esponente intero
+            Console.WriteLine("\nPotenza di 0 alla 3: " + CalcoliHelper.Potenza(0, 3));
+            //con base uguale a zero ed esponente negativo
+            Console.WriteLine("\nPotenza di 0 alla -3: " + CalcoliHelper.Potenza(0, -3));
+            //con base ed esponente uguale a zero
+            Console.WriteLine("\nPotenza di 0 alla 0: " + CalcoliHelper.Potenza(0, 0));
+            // con base ed esponenti positivi
+            Console.WriteLine("\nPotenza di 5 alla 3: " + CalcoliHelper.Potenza(5, 3));
+            //con base positiva ed esponente negativo
+            Console.WriteLine("\nPotenza di 5 alla -3: " + CalcoliHelper.Potenza(5, -3));
+            //con base negativa ed esponente positivo
+            Console.WriteLine("\nPotenza di -5 alla 3: " + CalcoliHelper.Potenza(-5, 3));
+            // con base ed esponente negativi
+            Console.WriteLine("\nPotenza di -5 alla -3: " + CalcoliHelper.Potenza(-5, -3));
         }
     }
 }
