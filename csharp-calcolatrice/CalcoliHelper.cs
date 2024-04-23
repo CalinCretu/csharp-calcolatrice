77705f18940a6f8553930b2e67427277fd2a7887
CalinCretu <calincretu1412@gmail.com>
2024-04-23 14:13:17 +0200

    ADD - creazione classe CalcoliHelper e funzinoe Somma(int/double)

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        // Metodo per la somma di due numeri interi
+        public static int Somma(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Metodo per la somma di due numeri double
+        public static double Somma(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Somma
+            Console.WriteLine("Somma di 3 e 5: " + CalcoliHelper.Somma(3, 5));
+            Console.WriteLine("Somma di 2.5 e 3.7: " + CalcoliHelper.Somma(2.5, 3.7));
         }
     }
 }
